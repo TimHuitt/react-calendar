a95466577b6ff8375584e51d19d91f5ac0df918d
Tim Huitt <timhuitt@gmail.com>
2023-12-09 16:00:37 -0700

    add calendar grid

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,12 @@ import "./App.css"
 import Calendar from "../Calendar/Calendar.jsx"
 
 const App = () => {
-
+  const selectedMonth = 'December'
+  
+  
   return (
     <div className="App">
-      <Calendar />
+      <Calendar selectedMonth={ selectedMonth } />
     </div>
   );
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,15 +1,46 @@
 import holidays from "../../data/holidays.js"
 import Day from "../Day/Day.jsx"
+import "./Calendar.css"
 
-const Calendar = () => {
+const Calendar = ({ selectedMonth }) => {
+
+  const year = '2024'
+  const date = new Date()
+  const first = new Date(date.getFullYear(), date.getMonth(), 1);
+  const numDays = new Date(year, selectedMonth, 0).getDate();
+  console.log(first.toDateString().split(' ')[0])
+  const totalCells = 7 * 5
+  const cellNumbers = Array.from({ length: totalCells }, (_, index) => index + 1)
+
   return (
-    <div key='Calendar'>
-    {Object.entries(holidays).map(([month, days]) => (
-      <div>
-        <h1>{month}</h1>
-        <Day days={days} />
+    <div className="Calendar" key="Calendar">
+
+      <div className="cal-wrapper">
+        {cellNumbers.map((cell) => (
+          <div className="cell">
+            {cell}
+          </div>
+        ))}
       </div>
-    ))}
+
+
+      {Object.entries(holidays).map(([month, days]) => (
+        <div key={month}>
+          {month === selectedMonth ? (
+            <>
+              <h1>{month}</h1>
+              <Day days={days} />
+            </>
+          ) : (
+            selectedMonth === 'all' && (
+              <>
+                <h1>{month}</h1><p>{numDays}</p>
+                <Day days={days} />
+              </>
+            )
+          )}
+        </div>
+      ))}
     </div>
   )
 }
